7fb49c5fb03be549cd5794d7cbe4ffc849d04c35
Simo Lin <linsimo.mark@gmail.com>
2026-03-10 18:54:19 -0700

    chore: fix lint (#720)
    Signed-off-by: Simo Lin <linsimo.mark@gmail.com>
    Signed-off-by: Sydney Firmin <sydney.firmin@oracle.com>

--- scripts/installation/install-sglang.sh
+++ scripts/installation/install-sglang.sh
@@ -9,6 +9,6 @@ SGL_SRC="${1:-/tmp/sglang-src}"
 cd "${SGL_SRC}/python"
 pip install --no-deps --force-reinstall --editable .
 
-# Pin gRPC packages to 1.78.0 — grpcio 1.78.1 was yanked from PyPI 
+# Pin gRPC packages to 1.78.0 — grpcio 1.78.1 was yanked from PyPI
 # TODO: remove when new SGLang 0.5.10 is released
 pip install --force-reinstall grpcio==1.78.0 grpcio-reflection==1.78.0 grpcio-tools==1.78.0 grpcio-health-checking==1.78.0
